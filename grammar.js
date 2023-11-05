1f2384be62035140866d0d26c8553baca038c01b
qvalentin <valentin.theodor@web.de>
2023-11-05 12:44:39 +0100

    Clean up rules

--- grammar.js
+++ grammar.js
@@ -133,16 +133,14 @@ module.exports = grammar({
             'else if',
             field('condition', $._pipeline),
             $._right_delimiter,
-            field('option', repeat($._else_if_clause_option)),
+            field('option', repeat($._block)),
         )),
-
-        _else_if_clause_option: $ => $._block,
 
         _else_clause: $ => prec.dynamic(PREC.else, seq(
             $._left_delimiter,
             'else',
             $._right_delimiter,
-            repeat(field('alternative', ($._block))),
+            (field('alternative', repeat($._block))),
         )),
 
         _if_actions_end: $ => seq(
